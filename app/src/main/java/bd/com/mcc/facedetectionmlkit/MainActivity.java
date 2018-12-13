2e3cf024f347c972da351bd1f53cb92bb61c246d
Mufad Monwar <monwar@mcc.com.bd>
2018-12-13 17:10:24 +0600

    first commit

---
+++ app/src/main/java/bd/com/mcc/facedetectionmlkit/MainActivity.java
@@ -0,0 +1,13 @@
+package bd.com.mcc.facedetectionmlkit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
